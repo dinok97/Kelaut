7ecd362792f423dd34a1c1e73a7ba7bdd80a799e
Dino Keylas <dinokeylas@gmail.com>
2021-03-14 14:58:32 +0700

    fix: change component name on home layout

--- app/src/main/java/com/kelaut/user/HomeFragment.kt
+++ app/src/main/java/com/kelaut/user/HomeFragment.kt
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kelaut.user.adapter.PromotionAdapter
 import com.kelaut.user.model.Promotion
 import com.kelaut.user.utils.Constant.Collection.Companion.PROMOTION
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlin.collections.ArrayList
 
 class HomeFragment : Fragment() {
 
@@ -24,6 +27,16 @@ class HomeFragment : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_home, container, false)
         ctx = view
+
+        view.cv_booking.setOnClickListener {
+            Toast.makeText(context, "booking clicked", Toast.LENGTH_SHORT).show()
+        }
+        view.cv_news.setOnClickListener {
+            Toast.makeText(context, "news clicked", Toast.LENGTH_SHORT).show()
+        }
+        view.cv_checklist.setOnClickListener {
+            Toast.makeText(context, "checklist clicked", Toast.LENGTH_SHORT).show()
+        }
 
         showPromotionList()
 
