35feecf95252e6e8952e58868196f145b14684ce
Dino Keylas <dinokeylas@gmail.com>
2021-03-23 22:08:27 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/kelaut/user/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.kelaut.user
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+
+    }
+
+}
